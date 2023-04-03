f09fce50025fb8b2f5fb61b20fcdbf90b67cd573
baeseungjun <bearwin98@gmail.com>
2023-04-04 00:47:12 +0900

    2.4_ex

---
+++ 2.4_ex.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char name[10];
+
+    cout<<"이름을 입력해주세요:";
+    cin>> name;
+
+    char tel[15];
+    cout<<"전화번화를 입력해주세요:";
+    cin>>tel;
+
+    cout<<endl<<"이름: "<<name<<endl;
+    cout<<"전화번호: "<<tel<<endl;
+
+    return 0;
+
+}
---
+++ UsingNameSpace_use.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"using namespace 사용"<<"이거 사용하면 아주좋네?"<<endl;
+    cout<<"뭐가 좋냐면 "<<"std를 안써도 돼."<<"개꿀..."<<endl;
+
+    return 0;
+}
